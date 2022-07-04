58d492ec2fb61e6c771f32fe5f5618e6f5cd8d97
Tohar-L <tohareden@gmail.com>
2022-07-04 16:51:43 +0300

    exerciseXP

--- week-3/w3d1/exerciseXP/exerciseXP-1-script.js
+++ week-3/w3d1/exerciseXP/exerciseXP-1-script.js
@@ -7,16 +7,31 @@ document.body.children[1].lastElementChild.textContent = "Richard";
 
 //3. Change each first name of the two <ul>'s to your name.
 let nameElement = document.getElementsByClassName("list");
-let firstNameElement = nameElement.firstElementChild;
+let firstNameElement = nameElement[0].firstElementChild;
+firstNameElement.textContent = "Tohar";
 console.log(firstNameElement);
+let lastNameElement = nameElement[1].firstElementChild;
+lastNameElement.textContent = "Tohar";
+console.log(lastNameElement);
 
-for (let index = 0; index < 1; index++) {
-	firstNameElement = 'Tohar';
-}
-console.log(firstNameElement);
 
 // 4. Delete the <li> that contains the text node “Sarah”.
-document.body.lastChild[2][1] = del
+let list = document.querySelectorAll("li");
+let ul = nameElement[1];
+console.log(list);
+ul.removeChild(list[3]);
+// list[3].remove();
+// console.log(list[3]);
+// body.lastChild[2][1] = del
 
 
+//bonus:
+nameElement[0].classList.add("student_list", "university", "attendance");
+console.log(nameElement[0]);
+nameElement[1].classList.add("student_list");
+console.log(nameElement[1]);
+
+for (let i of nameElement) {
+	i.classList.add("student_list-2", "class-2");
+}
 
